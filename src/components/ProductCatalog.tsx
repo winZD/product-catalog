68c23e084bebde2ae856596ccf41087cce1067b2
winZD <puntica007@gmail.com>
2024-12-06 19:58:28 +0100

    fix

--- src/components/ProductCatalog.tsx
+++ src/components/ProductCatalog.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Product, ProductResponse } from "../model/product";
 import { ProductCard } from "./ProductCard";
 import { handleFilter, handleFilterServer } from "../utils/filterProducts";
@@ -7,15 +7,15 @@ import { priceRanges } from "../utils/ranges";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const ProductCatalog = () => {
-  const [data, setData] = useState<ProductResponse>();
-  const [filteredData, setFilteredData] = useState<ProductResponse>();
+  const [data, setData] = useState<ProductResponse | null>();
+  /*   const [filteredData, setFilteredData] = useState<ProductResponse>();
   const [searchQuery, setSearchQuery] = useState("");
   const [category, setCategory] = useState<string>("");
-  const [sortPrice, setSortPrice] = useState<{ value: string }>();
-  /*   const [categories, setCategories] = useState<Category[]>([]);
+  const [sortPrice, setSortPrice] = useState<{ value: string }>(); */
+  /* const [categories, setCategories] = useState<Category[]>([]);
 
-  const [selectedCategory, setSelectedCategory] = useState(""); */
-
+  const [selectedCategory, setSelectedCategory] = useState("");
+ */
   // Calculate paginated products
   /*   const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
   const startIndex = (currentPage - 1) * productsPerPage;
@@ -23,7 +23,7 @@ export const ProductCatalog = () => {
   const currentProducts = products.slice(startIndex, endIndex); */
   const [page, setPage] = useState(0);
   const limit = 20;
-  /*  useEffect(() => {
+  useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await fetch(
@@ -32,14 +32,14 @@ export const ProductCatalog = () => {
         const data = await response.json();
         //setData(data.products);
         setData(data);
-        setFilteredData(data);
+        /* setFilteredData(data); */
 
         console.log(data);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     };
-    const fetchCategories = async () => {
+    /*  const fetchCategories = async () => {
       try {
         const response = await fetch(
           "https://dummyjson.com/products/categories"
@@ -50,11 +50,10 @@ export const ProductCatalog = () => {
       } catch (error) {
         console.error("Error fetching categories:", error);
       }
-    };
+    }; */
 
     fetchProducts();
-    fetchCategories();
-  }, [page]); */
+  }, [page]);
 
   //const totalPages = Math.ceil(filteredData! && filteredData!.total! / limit);
 
@@ -62,7 +61,7 @@ export const ProductCatalog = () => {
     name: "",
     price: "",
   }); */
-  const fetchProducts = async (): Promise<ProductResponse> => {
+  /*   const fetchProducts = async (): Promise<ProductResponse> => {
     const response = await fetch(
       category
         ? `${category}${
@@ -78,7 +77,7 @@ export const ProductCatalog = () => {
     }
     return response.json();
   };
-
+ */
   const fetchCategories = async (): Promise<Category[]> => {
     const response = await fetch("https://dummyjson.com/products/categories");
     if (!response.ok) {
@@ -86,7 +85,7 @@ export const ProductCatalog = () => {
     }
     return response.json();
   };
-  const {
+  /*  const {
     data: products,
     error: productsError,
     isPending: productsLoading,
@@ -96,11 +95,11 @@ export const ProductCatalog = () => {
       page,
       limit,
       sortPrice?.value,
-      category /* searchQuery */,
+      category, searchQuery,
     ],
 
     queryFn: fetchProducts,
-  });
+  }); */
   const {
     data: categories,
     error: categoriesError,
@@ -109,19 +108,48 @@ export const ProductCatalog = () => {
     queryKey: ["categories"],
     queryFn: fetchCategories,
   });
-  const totalPages = Math.ceil(products! && products!.total! / limit);
-  if (productsLoading || categoriesLoading) return <p>Loading...</p>;
-  if (productsError || categoriesError) return <p>Error loading data!</p>;
+
+  const handleSubmit = async (
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData(event.currentTarget);
+    const searchQuery = formData.get("searchQuery") as string;
+    const category = formData.get("category") as string;
+    const sortPrice = formData.get("sortPrice") as string;
+    console.log({ searchQuery, category, sortPrice });
+    setPage(0);
+    const response = await fetch(
+      category
+        ? `${category}${
+            sortPrice ? `?sortBy=price&order=${sortPrice}&` : "?"
+          }limit=${limit}&skip=${page}&select=title,price,thumbnail,description`
+        : `https://dummyjson.com/products?limit=${limit}&skip=${page}${
+            sortPrice ? `&sortBy=price&order=${sortPrice}` : ""
+          }&select=title,price,thumbnail,description`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch products");
+    }
+    const data = await response.json();
+    setData(data);
+  };
+  const totalPages = Math.ceil(data! && data!.total! / limit);
+  //const totalPages = Math.ceil(products! && products!.total! / limit);
+  if (categoriesLoading) return <p>Loading...</p>;
+  if (categoriesError) return <p>Error loading data!</p>;
 
   return (
-    <div className="w-full">
+    <form className="w-full" onSubmit={handleSubmit}>
       <div className="grid p-2 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <div className="relative">
           <input
             type="text"
+            name="searchQuery"
             placeholder="Search..."
             className="w-full py-2 pl-10 pr-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            onChange={(e) => setSearchQuery(e.target.value)}
           />
           <svg
             className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
@@ -139,7 +167,7 @@ export const ProductCatalog = () => {
         </div>
         <select
           className="py-2 px-4 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          onChange={(e) => setCategory(e.target.value)}
+          name="category"
         >
           <option value="">All Categories</option>
           {categories!.map((cat) => (
@@ -150,7 +178,7 @@ export const ProductCatalog = () => {
         </select>
         <select
           className="py-2 px-4 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          onChange={(e) => setSortPrice({ value: e.target.value })}
+          name="sortPrice"
         >
           {[{ value: "" }, { value: "asc" }, { value: "desc" }].map(
             (option) => (
@@ -161,6 +189,7 @@ export const ProductCatalog = () => {
           )}
         </select>
         <button
+          type="submit"
           className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
           /* onClick={() =>
             setFilteredData(handleFilterServer(searchQuery, data!, sortBy))
@@ -170,33 +199,43 @@ export const ProductCatalog = () => {
         </button>
       </div>
       <div className="grid p-2 sm:grid-cols-2 lg:3 xl:grid-cols-4 justify-center gap-4">
-        {products.products.map((product) => (
-          <div key={product.id}>
-            <ProductCard
-              description={product.description}
-              thumbnail={product.thumbnail}
-              title={product.title}
-              price={product.price}
-            />
-          </div>
-        ))}
+        {data &&
+          data.products.map((product) => (
+            <div key={product.id}>
+              <ProductCard
+                description={product.description}
+                thumbnail={product.thumbnail}
+                title={product.title}
+                price={product.price}
+              />
+            </div>
+          ))}
       </div>
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center mt-4 gap-3">
         <button
           className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 disabled:bg-blue-400 disabled:cursor-not-allowed"
           onClick={() => setPage((old) => Math.max(old - 20, 0))}
           disabled={page === 0}
         >
-          Previous
+          Prev
+        </button>
+
+        <button
+          type="button"
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg"
+        >
+          {" "}
+          {page / limit + 1}
         </button>
         <button
-          className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 ml-2 disabled:bg-blue-400 disabled:cursor-not-allowed"
+          type="button"
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 disabled:bg-blue-400 disabled:cursor-not-allowed"
           onClick={() => setPage((old) => old + 20)}
           disabled={page / 20 + 1 >= totalPages}
         >
           Next
         </button>
       </div>
-    </div>
+    </form>
   );
 };
